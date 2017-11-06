0bb04b08d58964e5fc1e968760778e40d44d035a
Dhruv Singh <dhruv_9427@hotmail.com>
2017-11-06 22:19:05 +0000

    Added Android logging statements to the GameScreen class and created a GameScreen object integrated within ZBGame.

--- core/src/com/kilobolt/screens/GameScreen.java
+++ core/src/com/kilobolt/screens/GameScreen.java
@@ -1,45 +1,53 @@
 package com.kilobolt.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 
 /**
  * Created by DhruvSingh on 05/11/2017.
  */
 public class GameScreen implements Screen {
 
-
-    @Override
-    public void show() {
-
+    public GameScreen(){
+        Gdx.app.log("GameScreen", "Attached");
     }
 
     @Override
     public void render(float delta) {
-
+        //Sets colour to fill the screen, RGB = 10,15,230
+        Gdx.gl.glClearColor(10/255.0f, 15/255.0f, 230/255.0f, 1f);
+        //Fills the screen with selected colour
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
     @Override
     public void resize(int width, int height) {
-
+        Gdx.app.log("GameScreen", "resizing");
     }
 
     @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
+    public void show() {
+        Gdx.app.log("GameScreen", "show called");
     }
 
     @Override
     public void hide() {
+        Gdx.app.log("GameScreen", "hide called");
+    }
 
+    @Override
+    public void pause() {
+        Gdx.app.log("GameScreen", "pause called");
+    }
+
+    @Override
+    public void resume() {
+        Gdx.app.log("GameScreen", "resume called");
     }
 
     @Override
     public void dispose() {
-
+        //Leave blank
     }
 }
--- core/src/com/kilobolt/zombiebird/ZBGame.java
+++ core/src/com/kilobolt/zombiebird/ZBGame.java
@@ -6,11 +6,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.kilobolt.screens.GameScreen;
 
 public class ZBGame extends Game{
 
 	@Override
 	public void create() {
 		Gdx.app.log("ZBGame", "created");
+		setScreen(new GameScreen());
 	}
 }
