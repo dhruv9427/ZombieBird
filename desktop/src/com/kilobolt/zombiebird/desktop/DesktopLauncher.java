8a381238626610fbf12b0a978994058e69f84e1f
Dhruv Singh <dhruv_9427@hotmail.com>
2017-11-15 22:55:02 +0000

    Removed some comments from DesktopLauncher

--- desktop/src/com/kilobolt/zombiebird/desktop/DesktopLauncher.java
+++ desktop/src/com/kilobolt/zombiebird/desktop/DesktopLauncher.java
@@ -8,6 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		//DesktopLauncher
+		//Dhruv
 		config.title="Zombie Bird";
 		config.width=272;
 		config.height=408;
