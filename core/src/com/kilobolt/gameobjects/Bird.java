612791043f7f3931bb6aa360b9f4ba1fa3aed538
Dhruv Singh <dhruv_9427@hotmail.com>
2022-09-25 17:49:43 +0100

    //commit to test again

--- core/src/com/kilobolt/gameobjects/Bird.java
+++ core/src/com/kilobolt/gameobjects/Bird.java
@@ -39,7 +39,7 @@ public class Bird {
         }
 
         //CEILING CHECK
-        //This is a test
+        //This is a test again
         if(position.y<-13){
             position.y=-13;
             velocity.y = 0;
